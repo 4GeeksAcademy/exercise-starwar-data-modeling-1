f50fccbb404e6fbe5c13dfc5f8911836e34dfc03
Sblaise123 <127683538+Sblaise123@users.noreply.github.com>
2023-06-25 03:00:07 +0000

    DataStarwars1

--- src/models.py
+++ src/models.py
@@ -12,8 +12,8 @@ class User(Base):
     id = Column(Integer, primary_key= True)
     username = Column(String(250), unique = True, nullable = False)
     password = Column(String(250), nullable = False)
-    favorite_character = Column(String(250), nullable = True)
-    favorite_planet = Column(String(250), nullable = True)
+    favorite_character = relationship('Character', backref = 'User', uselist = False)
+    favorite_planet = relationship('Planet', backref = 'User', uselist = False)
     
 
 class Character(Base):
@@ -26,9 +26,15 @@ class Character(Base):
 class Planet(Base):
     __tablename__ = 'Planet'
     id = Column(Integer, primary_key = True)
-    name = Column(String(250), ForeignKey('user.favorite_planet'), unique = True, nullable = False)
+    name = Column(String(250), ForeignKey('User.favorite_planet'), unique = True, nullable = False)
     popluation = Column(Integer, nullable = False)
     climate = Column(String(250), unique = True, nullable = False)
+
+class Favorite(Base):
+    __tablename__ = 'Favorite'
+    id = Column(Integer, primary_key = True)
+    name = Column(String(250), nullable = False)
+    Favorite_id = Column(Integer, ForeignKey("User"))
 
    
 
